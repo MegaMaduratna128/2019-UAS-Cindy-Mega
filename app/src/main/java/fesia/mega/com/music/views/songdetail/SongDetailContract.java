17b24b3100d0d990e86d3e11c0bf61155850fda0
FesiaCindy99 <cindyraverti99@gmail.com>
2019-05-27 23:14:17 +0700

    Fourth Commit

--- app/src/main/java/fesia/mega/com/music/views/songdetail/SongDetailContract.java
+++ app/src/main/java/fesia/mega/com/music/views/songdetail/SongDetailContract.java
@@ -1,5 +1,7 @@
 package fesia.mega.com.music.views.songdetail;
 
+import fesia.mega.com.music.api.model.Track;
+
 public class SongDetailContract {
     interface View {
         void displayMessage(String message);
