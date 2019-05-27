c9824c15c917d2dcc8e43e397fbe5c778329bc15
FesiaCindy99 <cindyraverti99@gmail.com>
2019-05-27 23:24:35 +0700

    5 Commit

--- app/src/main/java/fesia/mega/com/music/views/songlist/SongAdapter.java
+++ app/src/main/java/fesia/mega/com/music/views/songlist/SongAdapter.java
@@ -1,5 +1,52 @@
 package fesia.mega.com.music.views.songlist;
 
-public class SongAdapter {
-    
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.MyViewHolder> {
+
+    @NonNull
+    @Override
+    public SongAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SongAdapter.MyViewHolder myViewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout row;
+        ImageView imgTrackArtwork;
+        TextView txtTrackName, txtArtistNameNGenre, txtPrice;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            row = (LinearLayout) view.findViewById(R.id.song_item_row);
+            imgTrackArtwork = (ImageView) view.findViewById(R.id.artwork);
+            txtTrackName = (TextView) view.findViewById(R.id.track_name);
+            txtArtistNameNGenre = (TextView) view.findViewById(R.id.artist_name_and_genre);
+            txtPrice = (TextView) view.findViewById(R.id.price);
+            row.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent detail = new Intent(context, SongDetailView.class);
+                    detail.putExtra("track", trackList.get(getAdapterPosition()));
+                    context.startActivity(detail);
+                }
+            });
+        }
+    }
 }
