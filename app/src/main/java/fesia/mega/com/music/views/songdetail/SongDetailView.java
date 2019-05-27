938a9afd72d099c1a57770418a6a0fa8c2c67faa
FesiaCindy99 <cindyraverti99@gmail.com>
2019-05-27 22:44:13 +0700

    Third Commit

---
+++ app/src/main/java/fesia/mega/com/music/views/songdetail/SongDetailView.java
@@ -0,0 +1,12 @@
+package fesia.mega.com.music.views.songdetail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SongDetailView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
