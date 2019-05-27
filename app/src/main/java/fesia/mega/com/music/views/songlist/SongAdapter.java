960c4def8f7a1de0359c7ce0e2675e0be93e9b2e
FesiaCindy99 <cindyraverti99@gmail.com>
2019-05-27 23:30:10 +0700

    6 Commit

--- app/src/main/java/fesia/mega/com/music/views/songlist/SongAdapter.java
+++ app/src/main/java/fesia/mega/com/music/views/songlist/SongAdapter.java
@@ -1,39 +1,36 @@
 package fesia.mega.com.music.views.songlist;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import fesia.mega.com.music.api.model.Track;
+import fesia.mega.com.music.R;
+import fesia.mega.com.music.views.songdetail.SongDetailView;
+
 public class SongAdapter extends RecyclerView.Adapter<SongAdapter.MyViewHolder> {
 
-    @NonNull
-    @Override
-    public SongAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
-    }
+    Context context;
+    private List<Track> trackList;
 
-    @Override
-    public void onBindViewHolder(@NonNull SongAdapter.MyViewHolder myViewHolder, int i) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    class MyViewHolder extends RecyclerView.ViewHolder {
         LinearLayout row;
         ImageView imgTrackArtwork;
         TextView txtTrackName, txtArtistNameNGenre, txtPrice;
 
-        public MyViewHolder(@NonNull View itemView) {
-            super(itemView);
+        MyViewHolder(View view) {
+            super(view);
             row = (LinearLayout) view.findViewById(R.id.song_item_row);
             imgTrackArtwork = (ImageView) view.findViewById(R.id.artwork);
             txtTrackName = (TextView) view.findViewById(R.id.track_name);
@@ -48,5 +45,33 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.MyViewHolder> 
                 }
             });
         }
+    }
+
+    SongAdapter(Context context, List<Track> trackList) {
+        this.context = context;
+        this.trackList = trackList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.song_item, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Track track = trackList.get(position);
+
+        final String artworkUrl = track.getArtworkUrl100();
+        Glide.with(context).load(artworkUrl).placeholder(R.drawable.ic_logo).into(holder.imgTrackArtwork);
+
+        holder.txtTrackName.setText(track.getTrackName());
+        holder.txtArtistNameNGenre.setText(track.getArtistName() + " | " + track.getPrimaryGenreName());
+        holder.txtPrice.setText(String.format("US $ %s", String.valueOf(track.getTrackPrice())));
+    }
+
+    @Override
+    public int getItemCount() {
+        return trackList.size();
     }
 }
